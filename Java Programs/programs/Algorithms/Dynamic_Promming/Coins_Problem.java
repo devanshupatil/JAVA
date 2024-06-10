01324bb987b0ccd93aa29db635cd9e86a2180ba2
Devnashu Patil <devanshupatil692@gmail.com>
2024-06-11 04:38:40 +0530

    Coins Problem

---
+++ Java Programs/programs/Algorithms/Dynamic_Promming/Coins_Problem.java
@@ -0,0 +1,27 @@
+package Algorithms.Dynamic_Promming;
+
+public class Coins_Problem {
+    public static void main(String agrs[]) {
+        int Amount = 15;
+        int Coins[] = { 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int Arr[][] = new int[Coins.length + 1][Amount + 1];
+
+        // Initialize
+        for (int i = 0; i <= Coins.length; i++) {
+            Arr[i][0] = 1; // One way to make amount 0
+        }
+
+        for (int i = 1; i <= Coins.length; i++) {
+            for (int j = 1; j <= Amount; j++) {
+                if (Coins[i - 1] > j) {
+                    Arr[i][j] = Arr[i - 1][j];
+                } else {
+                    Arr[i][j] = Arr[i - 1][j] + Arr[i][j - Coins[i - 1]];
+                }
+            }
+        }
+
+        System.out.println("The number of ways to change the amount " + Amount + " = " + Arr[Coins.length][Amount]);
+
+    }
+}
